f2550c22f17a1208c77c741b243913621503b61c
Kamil Richert <krichert@atlassian.com>
2024-10-14 16:29:58 +0200

    Added setting value when chanign one field

--- modul-3-forms/dynamic-form-app/src/App.tsx
+++ modul-3-forms/dynamic-form-app/src/App.tsx
@@ -6,8 +6,26 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  name: string;
+  surname: string;
+  age: number;
+  year: number;
+};
 
 function App() {
+  const { register, handleSubmit, getValues, setValue } = useForm<FormValues>();
+
+  const handleAgeChange = () => {
+    const values = getValues();
+    setValue("year", 2024 - values.age);
+  };
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Box
       component="form"
@@ -19,22 +37,29 @@ function App() {
       }}
       noValidate
       autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Typography variant="h4" marginBottom={5}>
         Sign to our app
       </Typography>
 
       <FormLabel>Name</FormLabel>
-      <TextField margin="normal" />
+      <TextField margin="normal" {...register("name")} />
 
       <FormLabel>Surname</FormLabel>
-      <TextField margin="normal" />
+      <TextField margin="normal" {...register("surname")} />
 
       <FormLabel>Age</FormLabel>
-      <TextField margin="normal" />
+      <TextField
+        margin="normal"
+        {...register("age", {
+          valueAsNumber: true,
+          onChange: handleAgeChange,
+        })}
+      />
 
       <FormLabel>Year of birth</FormLabel>
-      <TextField margin="normal" />
+      <TextField margin="normal" {...register("year")} />
 
       <FormLabel>Hobbys</FormLabel>
       <Grid container marginTop={2}>

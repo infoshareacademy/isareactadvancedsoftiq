846ae7011965a8022de4bb65da6e09b74ebcfefc
Kamil Richert <krichert@atlassian.com>
2024-10-14 10:06:35 +0200

    Added vite config to enable camel case and used css module in services

--- modul-1-styling/modules-app/src/components/Services.jsx
+++ modul-1-styling/modules-app/src/components/Services.jsx
@@ -1,46 +1,52 @@
-import "./Services.css";
+import styles from "./Services.module.css";
 import iconCart from "../assets/icon-cart.png";
 import iconHand from "../assets/icon-hand.png";
 import iconWorld from "../assets/icon-world.png";
 import iconCarrot from "../assets/icon-carrot.png";
 
-export const Services = () => (
-  <section className="services">
-    <div className="container">
-      <h1 className="heading">Our Services</h1>
-      <div className="box">
-        <ul className="list">
-          <li className="list-item">
-            <a className="a-tag" href="#" target="_self">
-              Online Shopping{" "}
-              <img className="img" src={iconCart} alt="icon-cart" />
-            </a>
-          </li>
-          <li className="list-item">
-            <a className="a-tag" href="#" target="_self">
-              Quality Product{" "}
-              <img className="img" src={iconHand} alt="icon-hand" />
-            </a>
-          </li>
-          <li className="list-item">
-            <a className="a-tag" href="#" target="_self">
-              Domestic & International
-              <br /> Delivery{" "}
-              <img
-                className="img icon-world"
-                src={iconWorld}
-                alt="icon-world"
-              />
-            </a>
-          </li>
-          <li className="list-item">
-            <a className="a-tag" href="#" target="_self">
-              Well Organized{" "}
-              <img className="img" src={iconCarrot} alt="icon-carrot" />
-            </a>
-          </li>
-        </ul>
+export const Services = () => {
+  return (
+    <section className={styles.services}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Our Services</h1>
+        <div className={styles.box}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <a className={styles.aTag} href="#" target="_self">
+                Online Shopping{" "}
+                <img className={styles.img} src={iconCart} alt="icon-cart" />
+              </a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.aTag} href="#" target="_self">
+                Quality Product{" "}
+                <img className={styles.img} src={iconHand} alt="icon-hand" />
+              </a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.aTag} href="#" target="_self">
+                Domestic & International
+                <br /> Delivery{" "}
+                <img
+                  className={`${styles.img} ${styles.iconWorld}`}
+                  src={iconWorld}
+                  alt="icon-world"
+                />
+              </a>
+            </li>
+            <li className={styles.listItem}>
+              <a className={styles.aTag} href="#" target="_self">
+                Well Organized{" "}
+                <img
+                  className={styles.img}
+                  src={iconCarrot}
+                  alt="icon-carrot"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};

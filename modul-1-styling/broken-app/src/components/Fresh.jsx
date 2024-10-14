d71b397da63123b13fad6d8fa86643e205b27439
Kamil Richert <krichert@atlassian.com>
2024-10-14 09:19:06 +0200

    Dump fixing styling by adding component name

--- modul-1-styling/broken-app/src/components/Fresh.jsx
+++ modul-1-styling/broken-app/src/components/Fresh.jsx
@@ -3,14 +3,14 @@ import "./Fresh.css";
 export const Fresh = () => (
   <main>
     <section className="fresh">
-      <div className="container">
-        <div className="content">
-          <h2 className="title">Fresh Life</h2>
-          <p className="text">
+      <div className="fresh-container">
+        <div className="fresh-content">
+          <h2 className="fresh-title">Fresh Life</h2>
+          <p className="fresh-text">
             Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco.
           </p>
-          <button className="action">Get free now</button>
+          <button className="fresh-action">Get free now</button>
         </div>
       </div>
     </section>
--- modul-1-styling/broken-app/src/components/SiteHeader.jsx
+++ modul-1-styling/broken-app/src/components/SiteHeader.jsx
@@ -2,37 +2,37 @@ import "./SiteHeader.css";
 
 export const SiteHeader = () => (
   <header className="site-header">
-    <div className="container">
-      <h1 className="logo">Cahee</h1>
-      <nav className="nav">
-        <ul className="nav__menu">
-          <li className="nav__menu-item nav__menu-item-active">
-            <a className="nav__menu-a nav__menu--active" href="#">
+    <div className="site-header-container">
+      <h1 className="site-header-logo">Cahee</h1>
+      <nav className="site-header-nav">
+        <ul className="site-header-nav__menu">
+          <li className="site-header-nav__menu-item nav__menu-item-active">
+            <a className="site-header-nav__menu-a nav__menu--active" href="#">
               Home
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#about">
+          <li className="site-header-nav__menu-item">
+            <a className="site-header-nav__menu-a" href="#about">
               About
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#services">
+          <li className="site-header-nav__menu-item">
+            <a className="site-header-nav__menu-a" href="#services">
               Services
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#gallery">
+          <li className="site-header-nav__menu-item">
+            <a className="site-header-nav__menu-a" href="#gallery">
               Gallery
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#blog">
+          <li className="site-header-nav__menu-item">
+            <a className="site-header-nav__menu-a" href="#blog">
               Blog
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#contact">
+          <li className="site-header-nav__menu-item">
+            <a className="site-header-nav__menu-a" href="#contact">
               Contact
             </a>
           </li>

1d854893549e76096c5b2572e925596db82edb52
Kamil Richert <krichert@atlassian.com>
2024-10-14 13:00:36 +0200

    Solved ex 1 in typescript

--- modul-2-typescript/zadanie-1/index.ts
+++ modul-2-typescript/zadanie-1/index.ts
@@ -6,41 +6,71 @@ interface IUser {
     name: string;
 }
 
-
 // Rozszerzanie
 // Dodaj nowe pole "role", które moze przyjąć jedynie opcje "user" lub "admin"
 
+interface IUser {
+    role: "user" | "admin"
+}
+
+type NewUser = User & {
+    role: "user" | "admin"
+}
+
+interface INewUser extends User {
+    role: "user" | "admin"
+}
 
 
 // Nowy typ Admin
 // Stwórz nowy typ, który będzie zawierał wszytsko co ma poprzedni typ (ale "role" będzie tylko jedną opcją) 
 // i dodatkowo będzie pole "permissions" jako tablica ciągów znaku
 
-
+type Admin = IUser & {
+    role: "admin",
+    persmissions: string[]
+}
 
 // Stwórz typ Person który będzie albo User albo Admin
+
+type Person = Admin | User
 
 
 // --------------------------------------------------------
 
 // Poprawa FetchData tak by mozna sprecyzowac czym jest data
-type FetchData = {
+type FetchData<T> = {
     isLoading: boolean;
     error: Error | null;
-    data: any[]
+    data: T
 }
 
+const result: FetchData<string[]> = {
+    isLoading: false,
+    error: null,
+    data: ['a', 'b', 'c']
+}
 
 // Stwórz typ Store który przyjmuje dwa parametry, 
 // który decdydują jaki typ ma data przechowywana oraz
 // oraz jaki typ ma stack w type StoreError
 
-type StoreError = {
+type StoreError<T> = {
     message: string;
-    stack: any;
+    stack: T;
 }
 
-type Store = {
-    data: any;
-    error: any;
+type Store<D, ES> = {
+    data: D;
+    error: StoreError<ES>;
+}
+
+const store: Store<{ key: string }, string> = {
+    data: {
+        key: 'softiq'
+    },
+    error: {
+        message: 'message',
+        stack: 'to jest stack error'
+    }
 }

c6459a1c93898d03abce87de6180afd1796aa5a1
Kamil Richert <krichert@atlassian.com>
2024-10-15 09:09:11 +0200

    Added generating dynamic fields

--- modul-3-forms/dynamic-form-app/src/App.tsx
+++ modul-3-forms/dynamic-form-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import {
   TextField,
   FormLabel,
@@ -6,22 +7,31 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, useFieldArray } from "react-hook-form";
 
 type FormValues = {
   name: string;
   surname: string;
   age: number;
   year: number;
+  hobbys: { name: string }[];
 };
 
 function App() {
-  const { register, handleSubmit, getValues, setValue } = useForm<FormValues>();
+  const [newHobby, setNewHobby] = useState("");
+  const { register, handleSubmit, getValues, setValue, control } =
+    useForm<FormValues>();
+  const { append, remove, fields } = useFieldArray({ control, name: "hobbys" });
 
   const handleAgeChange = () => {
     const values = getValues();
     setValue("year", 2024 - values.age);
   };
+
+  const handleAppendHobby = () => {
+    append({ name: newHobby });
+  };
+
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
   };
@@ -42,13 +52,10 @@ function App() {
       <Typography variant="h4" marginBottom={5}>
         Sign to our app
       </Typography>
-
       <FormLabel>Name</FormLabel>
       <TextField margin="normal" {...register("name")} />
-
       <FormLabel>Surname</FormLabel>
       <TextField margin="normal" {...register("surname")} />
-
       <FormLabel>Age</FormLabel>
       <TextField
         margin="normal"
@@ -57,23 +64,43 @@ function App() {
           onChange: handleAgeChange,
         })}
       />
-
       <FormLabel>Year of birth</FormLabel>
       <TextField margin="normal" {...register("year")} />
-
       <FormLabel>Hobbys</FormLabel>
-      <Grid container marginTop={2}>
-        <Grid item>
-          <TextField />
+      {fields.map((field, index) => (
+        <Grid container marginTop={2}>
+          <Grid item>
+            <TextField {...register(`hobbys.${index}.name`)} />
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => remove(index)}
+            >
+              X
+            </Button>
+          </Grid>
         </Grid>
+      ))}
 
+      <Grid container marginTop={2}>
+        <Grid item alignItems="stretch">
+          <TextField
+            value={newHobby}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setNewHobby(e.target.value)
+            }
+          />
+        </Grid>
         <Grid item alignItems="stretch" style={{ display: "flex" }}>
-          <Button color="primary" variant="contained">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleAppendHobby}
+          >
             Add
           </Button>
         </Grid>
       </Grid>
-
       <Button type="submit">Send</Button>
     </Box>
   );

0c466540bc76255675f776487d3849ee45b5a581
Kamil Richert <krichert@atlassian.com>
2024-10-16 14:40:09 +0200

    Redux provider

--- modul-7-state/reducer-app/src/Counter.jsx
+++ modul-7-state/reducer-app/src/Counter.jsx
@@ -1,11 +1,21 @@
+import { ReduxContext } from "./ReduxProvider";
+import { useContext } from "react";
+
 export const Counter = () => {
-  const increment = () => {};
-  const decrement = () => {};
-  const reset = () => {};
+  const [state, dispatch] = useContext(ReduxContext);
+  const increment = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
+  const reset = () => {
+    dispatch({ type: "reset" });
+  };
 
   return (
     <div>
-      <h1>Counter: 0</h1>
+      <h1>Counter: {state.value}</h1>
       <button onClick={increment}>Zwiększ</button>
       <button onClick={decrement}>Zmniejsz</button>
       <button onClick={reset}>Resetuj</button>
--- modul-7-state/reducer-app/src/ReduxProvider.jsx
+++ modul-7-state/reducer-app/src/ReduxProvider.jsx
@@ -1,7 +1,29 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const ReduxContext = createContext({ state: 0, dispatch: () => {} });
+function reducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case "increment":
+      // nie dziala
+      //   state.value = state.value + 1;
+      //   return state;
+      return { value: state.value + 1 };
+    case "decrement":
+      return { value: state.value - 1 };
+    case "reset":
+      return { value: 0 };
+    default:
+      return state;
+  }
+}
+
+export const ReduxContext = createContext([{ value: 0 }, () => {}]);
 
 export const ReduxProvider = ({ children }) => {
-  return <ReduxContext.Provider>{children}</ReduxContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <ReduxContext.Provider value={[state, dispatch]}>
+      {children}
+    </ReduxContext.Provider>
+  );
 };

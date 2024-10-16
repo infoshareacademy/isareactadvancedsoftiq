d2fe4b02dc17fcf177e0b5e94935b541131797b6
Kamil Richert <krichert@atlassian.com>
2024-10-16 09:14:17 +0200

    Added lazy laoding

--- modul-6-optimisation/lazy-app/src/content/index.tsx
+++ modul-6-optimisation/lazy-app/src/content/index.tsx
@@ -1,14 +1,34 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./home";
-import { Menu } from "./menu";
-import { Details } from "./details";
-import { Admin } from "./admin";
+import { lazy, Suspense } from "react";
+import { PageWrapper } from "../common/page-wrapper";
+import { CircularProgress } from "@mui/material";
+
+const LazyHome = lazy(() =>
+  import("./home").then((module) => ({ default: module.Home }))
+);
+const LazyMenu = lazy(() =>
+  import("./menu").then((module) => ({ default: module.Menu }))
+);
+const LazyDetails = lazy(() =>
+  import("./details").then((module) => ({ default: module.Details }))
+);
+const LazyAdmin = lazy(() =>
+  import("./admin").then((module) => ({ default: module.Admin }))
+);
 
 export const Content = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/menu" element={<Menu />} />
-    <Route path="/menu/:id" element={<Details />} />
-    <Route path="/admin" element={<Admin />} />
-  </Routes>
+  <Suspense
+    fallback={
+      <PageWrapper>
+        <CircularProgress />
+      </PageWrapper>
+    }
+  >
+    <Routes>
+      <Route path="/" element={<LazyHome />} />
+      <Route path="/menu" element={<LazyMenu />} />
+      <Route path="/menu/:id" element={<LazyDetails />} />
+      <Route path="/admin" element={<LazyAdmin />} />
+    </Routes>
+  </Suspense>
 );
